82e46f1f9348737abe5d13ccf4761185a7d5c8e0
Sisilisko <beloborodova.viktoriia@gmail.com>
2023-05-06 21:38:35 +0300

    init: started project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import { content } from "../Content"
+
+const Hero = () => {
+  const { hero } = content;
+  return <section>
+    <div className="min-h-screen relative">
+      <div className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bottom-0 bg-primaryLinear"></div>
+      <h1>
+        {hero.firstName}
+        <span>{hero.LastName}</span>
+      </h1>
+    </div>
+  </section>;
+};
+
+export default Hero;
